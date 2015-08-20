bd8c237926ddc078e82abca74c088c122631132b
Stephen Scott <scottie1984@gmail.com>
2015-08-20 15:05:25 +0100

    Added connection pooling so it doesnt keep connecting

--- src/connection.js
+++ src/connection.js
@@ -1,16 +1,25 @@
-"use strict";
+'use strict';
 
 import { MongoClient } from 'mongodb';
 import * as Q from 'q';
+import * as R from 'ramda';
+
+let connectionPool = {};
 
 export function getConnection(connectionString) {
     return new Promise((resolve, reject) => {
-        MongoClient.connect(connectionString, (err, db) => {
-            if(err) {
-                reject(err);
-            } else {
-                resolve(db);
-            }
-        });
+        let hasConnection = R.pick([connectionString], connectionPool);
+        if (R.keys(hasConnection).length !== 0) {
+            resolve(connectionPool[connectionString]);
+        } else {
+            MongoClient.connect(connectionString, (err, db) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    connectionPool[connectionString] = db;
+                    resolve(db);
+                }
+            });
+        }
     });
 }
--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
-var assert = require("assert");
-var mf = require("../lib");
-var R = require("ramda");
+var assert = require('assert');
+var mf = require('../lib');
+var R = require('ramda');
 
 describe('Mongo', function() {
 
-  var findOner = mf.findOne("mongodb://localhost:27017/mongo-func");
-  var finder = mf.find("mongodb://localhost:27017/mongo-func");
-  var inserter = mf.insert("mongodb://localhost:27017/mongo-func");
-  var updater = mf.update("mongodb://localhost:27017/mongo-func");
-  var remover = mf.remove("mongodb://localhost:27017/mongo-func");
+  var findOner = mf.findOne('mongodb://localhost:27017/mongo-func');
+  var finder = mf.find('mongodb://localhost:27017/mongo-func');
+  var inserter = mf.insert('mongodb://localhost:27017/mongo-func');
+  var updater = mf.update('mongodb://localhost:27017/mongo-func');
+  var remover = mf.remove('mongodb://localhost:27017/mongo-func');
 
   var findOneFromTest = findOner('test');
   var finderFromTest = finder('test');
@@ -17,19 +17,19 @@ describe('Mongo', function() {
   var removeFromTest = remover('test');
 
   beforeEach(function(done) {
-      mf.dropCollection("mongodb://localhost:27017/mongo-func", 'test')().then(function(){
+      mf.dropCollection('mongodb://localhost:27017/mongo-func', 'test')().then(function(){
         done();
       }).catch(function() {
         done();
-      })
+      });
   });
 
   describe('connection error', function () {
       it('should return an error when it cannot connect to mongo', function (done) {
-        mf.findOne("mongodb://hostnothere:999/mongo-func", 'test', {})().then(function(result) {
+        mf.findOne('mongodb://hostnothere:999/mongo-func', 'test', {})().then(function(result) {
 
         }).catch(function(err) {
-           assert.equal(err, "Error: failed to connect to [hostnothere:999]");
+           assert.equal(err, 'Error: failed to connect to [hostnothere:999]');
            done();
         });
       });
@@ -37,16 +37,16 @@ describe('Mongo', function() {
 
   describe('findOne (non-curry)', function () {
     it('should return null when document not found', function (done) {
-      mf.findOne("mongodb://localhost:27017/mongo-func", 'test', {})().then(function(docs) {
+      mf.findOne('mongodb://localhost:27017/mongo-func', 'test', {})().then(function(docs) {
          assert.equal(docs, null);
          done();
       });
     });
 
     it('should return document when document is found', function (done) {
-      mf.insert("mongodb://localhost:27017/mongo-func", 'test', {scooby: "doo"})().then(function(result) {
-           mf.findOne("mongodb://localhost:27017/mongo-func", 'test', {scooby: "doo"})().then(function(doc) {
-                assert.equal(doc.scooby, "doo");
+      mf.insert('mongodb://localhost:27017/mongo-func', 'test', {scooby: 'doo'})().then(function(result) {
+           mf.findOne('mongodb://localhost:27017/mongo-func', 'test', {scooby: 'doo'})().then(function(doc) {
+                assert.equal(doc.scooby, 'doo');
                 done();
            });
       });
@@ -56,68 +56,68 @@ describe('Mongo', function() {
   describe('findOne (curry)', function () {
 
       it('should return document when document is found', function (done) {
-        inserter('test', {scooby: "doo"})().then(function(result) {
-             findOner('test', {scooby: "doo"})().then(function(doc) {
-                  assert.equal(doc.scooby, "doo");
+        inserter('test', {scooby: 'doo'})().then(function(result) {
+             findOner('test', {scooby: 'doo'})().then(function(doc) {
+                  assert.equal(doc.scooby, 'doo');
                   done();
-             });;
+             });
         });
       });
 
       it('should return document when document is found', function (done) {
-          insertToTest({scooby: "doo"})().then(function(result) {
-               findOneFromTest({scooby: "doo"})().then(function(doc) {
-                    assert.equal(doc.scooby, "doo");
+          insertToTest({scooby: 'doo'})().then(function(result) {
+               findOneFromTest({scooby: 'doo'})().then(function(doc) {
+                    assert.equal(doc.scooby, 'doo');
                     done();
-               });;
+               });
           });
       });
 
       it('should allow a function to be passed into finder', function (done) {
-            var byScooby = function(name) { return { scooby : name } };
+            var byScooby = function(name) { return { scooby : name }; };
             var findOneFromTestByScooby = findOneFromTest(byScooby);
-            insertToTest({scooby: "doo"})().then(function(result) {
-                 findOneFromTestByScooby("doo").then(function(doc) {
-                     assert.equal(doc.scooby, "doo");
-                     findOneFromTestByScooby("woo").then(function(doc2) {
+            insertToTest({scooby: 'doo'})().then(function(result) {
+                 findOneFromTestByScooby('doo').then(function(doc) {
+                     assert.equal(doc.scooby, 'doo');
+                     findOneFromTestByScooby('woo').then(function(doc2) {
                         assert.equal(doc2, undefined);
                         done();
                      });
-                 });;
+                 });
             });
       });
 
       it('should allow a function with multiple parameters to be passed into finder', function (done) {
-          var byScoobyAndLive = function(name, isLive) { return { scooby : name, live : isLive } };
+          var byScoobyAndLive = function(name, isLive) { return { scooby : name, live : isLive }; };
           var findOneFromTestByScoobyAndLive = findOneFromTest(byScoobyAndLive);
-          insertToTest({scooby: "doo", live: true})().then(function(result) {
-               findOneFromTestByScoobyAndLive("doo", true).then(function(doc) {
-                   assert.equal(doc.scooby, "doo");
-                   findOneFromTestByScoobyAndLive("doo", false).then(function(doc2) {
+          insertToTest({scooby: 'doo', live: true})().then(function(result) {
+               findOneFromTestByScoobyAndLive('doo', true).then(function(doc) {
+                   assert.equal(doc.scooby, 'doo');
+                   findOneFromTestByScoobyAndLive('doo', false).then(function(doc2) {
                       assert.equal(doc2, undefined);
                       done();
                    });
-               });;
+               });
           });
       });
 
       it('should allow a function with multiple parameters to be passed and curried into finder', function (done) {
-            var byScoobyAndLive = function(name, isLive) { return { scooby : name, live : isLive } };
+            var byScoobyAndLive = function(name, isLive) { return { scooby : name, live : isLive }; };
             var findOneFromTestByScoobyAndLive = findOneFromTest(byScoobyAndLive);
-            var findOneFromTestByScoobyDooAndLive = findOneFromTestByScoobyAndLive("doo");
-            insertToTest({scooby: "doo", live: true})().then(function(result) {
+            var findOneFromTestByScoobyDooAndLive = findOneFromTestByScoobyAndLive('doo');
+            insertToTest({scooby: 'doo', live: true})().then(function(result) {
                  findOneFromTestByScoobyDooAndLive(true).then(function(doc) {
-                     assert.equal(doc.scooby, "doo");
+                     assert.equal(doc.scooby, 'doo');
                      done();
-                 });;
+                 });
             });
       });
 
       it('should allow to curry the returning function', function (done) {
-            insertToTest({scooby: "doo", scrappy: "don't"})().then(function(result) {
-                 findOneFromTest({scooby: "doo"})().then(R.pick(['scrappy'])).then(function(doc){
+            insertToTest({scooby: 'doo', scrappy: 'don"t'})().then(function(result) {
+                 findOneFromTest({scooby: 'doo'})().then(R.pick(['scrappy'])).then(function(doc){
                      assert.equal(doc.scooby, undefined);
-                     assert.equal(doc.scrappy, "don't");
+                     assert.equal(doc.scrappy, 'don"t');
                      done();
                  });
             });
@@ -127,27 +127,27 @@ describe('Mongo', function() {
   describe('update', function () {
 
       it('should update a document matching the query', function (done) {
-          insertToTest({scooby: "doo", scrappy: "woo"})()
-              .then(updateToTest({scooby: "doo"}, {$set: {scooby: "too"}}))
-              .then(findOneFromTest({scooby: "too"}))
+          insertToTest({scooby: 'doo', scrappy: 'woo'})()
+              .then(updateToTest({scooby: 'doo'}, {$set: {scooby: 'too'}}))
+              .then(findOneFromTest({scooby: 'too'}))
               .then(function(doc) {
-                        assert.equal(doc.scooby, "too");
-                        assert.equal(doc.scrappy, "woo");
+                        assert.equal(doc.scooby, 'too');
+                        assert.equal(doc.scrappy, 'woo');
                         done();
                     });
       });
 
-      var updateToTestForScoobyDoo = updateToTest({scooby: "doo"});
+      var updateToTestForScoobyDoo = updateToTest({scooby: 'doo'});
 
       it('should update a document matching the query using function compose and currying', function (done) {
 
-          var insertAndUpdate = R.composeP(findOneFromTest({scooby: "too"}),
-                                        updateToTestForScoobyDoo({$set: {scooby: "too"}}),
-                                        insertToTest({scooby: "doo", scrappy: "woo"}))
+          var insertAndUpdate = R.composeP(findOneFromTest({scooby: 'too'}),
+                                        updateToTestForScoobyDoo({$set: {scooby: 'too'}}),
+                                        insertToTest({scooby: 'doo', scrappy: 'woo'}));
           insertAndUpdate()
               .then(function(doc) {
-                        assert.equal(doc.scooby, "too");
-                        assert.equal(doc.scrappy, "woo");
+                        assert.equal(doc.scooby, 'too');
+                        assert.equal(doc.scrappy, 'woo');
                         done();
                     });
       });
@@ -158,15 +158,15 @@ describe('Mongo', function() {
 
     it('should return all documents matching query', function (done) {
 
-        var insertAndFindScoobyDoo = R.composeP(finderFromTest({scooby: "doo"}),
-                                      insertToTest({scooby: "dont", scrappy: "foo"}),
-                                      insertToTest({scooby: "doo", scrappy: "loo"}),
-                                      insertToTest({scooby: "doo", scrappy: "woo"}))
+        var insertAndFindScoobyDoo = R.composeP(finderFromTest({scooby: 'doo'}),
+                                      insertToTest({scooby: 'dont', scrappy: 'foo'}),
+                                      insertToTest({scooby: 'doo', scrappy: 'loo'}),
+                                      insertToTest({scooby: 'doo', scrappy: 'woo'}));
         insertAndFindScoobyDoo()
             .then(function(docs) {
                       assert.equal(docs.length, 2);
-                      assert.equal(docs[0].scrappy, "woo");
-                      assert.equal(docs[1].scrappy, "loo");
+                      assert.equal(docs[0].scrappy, 'woo');
+                      assert.equal(docs[1].scrappy, 'loo');
                       done();
                   });
     });
@@ -177,20 +177,20 @@ describe('Mongo', function() {
 
       it('should remove a document from the collection', function (done) {
 
-          var insert2Docs = R.composeP(insertToTest({scooby: "doo", scrappy: "foo"}),
-                                        insertToTest({scooby: "doo", scrappy: "woo"}))
+        var insert2Docs = R.composeP(insertToTest({scooby: 'doo', scrappy: 'foo'}),
+                                        insertToTest({scooby: 'doo', scrappy: 'woo'}));
           insert2Docs()
-              .then(finderFromTest({scooby: "doo"}))
+              .then(finderFromTest({scooby: 'doo'}))
               .then(function(docs) {
-                 assert.equal(docs.length, 2);
+                assert.equal(docs.length, 2);
               })
-              .then(removeFromTest({scrappy: "foo"}))
-              .then(finderFromTest({scooby: "doo"}))
+              .then(removeFromTest({scrappy: 'foo'}))
+              .then(finderFromTest({scooby: 'doo'}))
               .then(function(docs) {
                  assert.equal(docs.length, 1);
-                 assert.equal(docs[0].scrappy, "woo");
+                 assert.equal(docs[0].scrappy, 'woo');
                  done();
-              })
+              });
       });
 
   });
