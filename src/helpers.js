170fb401e12b55d32a6d8d9400363b1b48197998
Daniele Sangalli <sangallidaniele@gmail.com>
2017-11-07 13:17:27 +0000

    - updated git ignore - added support to eslint and removed jshint - reformatted code with prettier

--- src/helpers.js
+++ src/helpers.js
@@ -1,57 +1,79 @@
-import * as R from 'ramda';
-import { connectToCollection, getConnection } from './connection';
+import * as R from "ramda";
+import { connectToCollection, getConnection } from "./connection";
 
 export let resultResolver = (resolve, reject) => {
-    return (err, result) => {
-       if (err) {
-          reject(err);
-       } else {
-          resolve(result);
-       }
-    };
+  return (err, result) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  };
 };
 
 export let runner = R.curry((fn, connectionString, collectionName) => {
-    return () => {
-        return mongoFnRunner(fn, collectionName, connectionString);
-    };
+  return () => {
+    return mongoFnRunner(fn, collectionName, connectionString);
+  };
 });
 
-export let funcBuilder = R.curry((mongoFn, input, connectionString, collectionName) => {
+export let funcBuilder = R.curry(
+  (mongoFn, input, connectionString, collectionName) => {
     return R.curryN(input.length, (...args) => {
-        let inputValue = input.apply(null,args);
-        return mongoFnRunner(mongoFn(inputValue), collectionName, connectionString);
+      let inputValue = input.apply(null, args);
+      return mongoFnRunner(
+        mongoFn(inputValue),
+        collectionName,
+        connectionString
+      );
     });
-});
+  }
+);
 
-export let funcBuilder2 = R.curry((mongoFn, query, update, connectionString, collectionName) => {
+export let funcBuilder2 = R.curry(
+  (mongoFn, query, update, connectionString, collectionName) => {
     return R.curryN(query.length, (...args) => {
-        let queryValue = query.apply(null,args);
-        if (typeof update === 'object') {
-           return mongoFnRunner(mongoFn(queryValue, update), collectionName, connectionString);
-        } else {
-            return R.curryN(query.length, (...args) => {
-               let updateValue = update.apply(null,args);
-               return mongoFnRunner(mongoFn(queryValue, updateValue), collectionName, connectionString);
-            });
-        }
+      let queryValue = query.apply(null, args);
+      if (typeof update === "object") {
+        return mongoFnRunner(
+          mongoFn(queryValue, update),
+          collectionName,
+          connectionString
+        );
+      } else {
+        return R.curryN(query.length, (...args) => {
+          let updateValue = update.apply(null, args);
+          return mongoFnRunner(
+            mongoFn(queryValue, updateValue),
+            collectionName,
+            connectionString
+          );
+        });
+      }
     });
-});
+  }
+);
 
-export function createInputFn(input){
-    let inputFn = input;
-    if (typeof input === 'object' || typeof input ===  'string') {
-       inputFn = function() { return input; };
-    }
-    return inputFn;
+export function createInputFn(input) {
+  let inputFn = input;
+  if (typeof input === "object" || typeof input === "string") {
+    inputFn = function() {
+      return input;
+    };
+  }
+  return inputFn;
 }
 
 function mongoFnRunner(fn, collectionName, connectionString) {
-    let connector = R.composeP(fn, connectToCollection(collectionName));
-    return connector(connectionString);
+  let connector = R.composeP(fn, connectToCollection(collectionName));
+  return connector(connectionString);
 }
 
 export function stats(connectionString) {
-  return getConnection(connectionString)
-    .then(db => new Promise((resolve, reject) => db.stats(resultResolver(resolve, reject))));
+  return getConnection(connectionString).then(
+    db =>
+      new Promise((resolve, reject) =>
+        db.stats(resultResolver(resolve, reject))
+      )
+  );
 }
--- src/mongo.js
+++ src/mongo.js
@@ -1,110 +1,148 @@
-import * as helpers from './helpers';
-import * as R from 'ramda';
+import * as helpers from "./helpers";
+import * as R from "ramda";
 
-let findOneDoc = R.curry( (document, collection) => {
-    return new Promise((resolve, reject) => {
-        collection.findOne(document, helpers.resultResolver(resolve, reject));
-    });
+let findOneDoc = R.curry((document, collection) => {
+  return new Promise((resolve, reject) => {
+    collection.findOne(document, helpers.resultResolver(resolve, reject));
+  });
 });
 
-let findDoc = R.curry( (document, collection) => {
-	let operationDefinitions = ['limit', 'sort', 'skip'];
-    let operations = R.pick(operationDefinitions, document);
-    document = R.omit(operationDefinitions, document);
+let findDoc = R.curry((document, collection) => {
+  let operationDefinitions = ["limit", "sort", "skip"];
+  let operations = R.pick(operationDefinitions, document);
+  document = R.omit(operationDefinitions, document);
 
-    return new Promise((resolve, reject) => {
-        let find = collection.find(document);
-        R.mapObjIndexed((val, key) => {
-           find = find[key](val);
-        }, operations);
+  return new Promise((resolve, reject) => {
+    let find = collection.find(document);
+    R.mapObjIndexed((val, key) => {
+      find = find[key](val);
+    }, operations);
 
-        find.toArray(helpers.resultResolver(resolve, reject));
-    });
+    find.toArray(helpers.resultResolver(resolve, reject));
+  });
 });
 
-let countDoc = R.curry( (document, collection) => {
-    return new Promise((resolve, reject) => {
-        collection.count(document, helpers.resultResolver(resolve, reject));
-    });
+let countDoc = R.curry((document, collection) => {
+  return new Promise((resolve, reject) => {
+    collection.count(document, helpers.resultResolver(resolve, reject));
+  });
 });
 
-let insertDoc = R.curry( (document, collection) => {
-    return new Promise((resolve, reject) => {
-        collection.insert(document, helpers.resultResolver(resolve, reject));
-    });
+let insertDoc = R.curry((document, collection) => {
+  return new Promise((resolve, reject) => {
+    collection.insert(document, helpers.resultResolver(resolve, reject));
+  });
 });
 
-let updateDoc = R.curry( (query, update, collection) => {
-    return new Promise((resolve, reject) => {
-        collection.update(query, update, helpers.resultResolver(resolve, reject));
-    });
+let updateDoc = R.curry((query, update, collection) => {
+  return new Promise((resolve, reject) => {
+    collection.update(query, update, helpers.resultResolver(resolve, reject));
+  });
 });
 
-let removeDoc = R.curry( (document, collection) => {
-    return new Promise((resolve, reject) => {
-        collection.remove(document, helpers.resultResolver(resolve, reject));
-    });
+let removeDoc = R.curry((document, collection) => {
+  return new Promise((resolve, reject) => {
+    collection.remove(document, helpers.resultResolver(resolve, reject));
+  });
 });
 
 let drop = collection => {
-    return new Promise((resolve, reject) => {
-        collection.drop(helpers.resultResolver(resolve, reject));
-    });
+  return new Promise((resolve, reject) => {
+    collection.drop(helpers.resultResolver(resolve, reject));
+  });
 };
 
-let distinctFields = R.curry( (field, collection) => {
-    return new Promise((resolve, reject) => {
-        collection.distinct(field, helpers.resultResolver(resolve, reject));
-    });
+let distinctFields = R.curry((field, collection) => {
+  return new Promise((resolve, reject) => {
+    collection.distinct(field, helpers.resultResolver(resolve, reject));
+  });
 });
 
 //input: String, String, Object OR Function (returns Object)
 //output: Function ( accepts parameters of query function )
 export var findOne = R.curry((connectionString, collectionName, query) => {
-    return helpers.funcBuilder(findOneDoc, helpers.createInputFn(query), connectionString, collectionName);
+  return helpers.funcBuilder(
+    findOneDoc,
+    helpers.createInputFn(query),
+    connectionString,
+    collectionName
+  );
 });
 
 //input: String, String, Object OR Function (returns Object)
 //output: Function ( accepts parameters of query function )
 export var find = R.curry((connectionString, collectionName, query) => {
-    return helpers.funcBuilder(findDoc, helpers.createInputFn(query), connectionString, collectionName);
+  return helpers.funcBuilder(
+    findDoc,
+    helpers.createInputFn(query),
+    connectionString,
+    collectionName
+  );
 });
 
 //input: String, String, Object OR Function (returns Object)
 //output: Function ( accepts parameters of query function )
 export var insert = R.curry((connectionString, collectionName, document) => {
-    return helpers.funcBuilder(insertDoc, helpers.createInputFn(document), connectionString, collectionName);
+  return helpers.funcBuilder(
+    insertDoc,
+    helpers.createInputFn(document),
+    connectionString,
+    collectionName
+  );
 });
 
 //input: String, String, Object OR Function (returns Object), Object OR Function (returns Object)
 //output: Function ( accepts parameters of query function )
 //if update is a Function then another Function is returned ( accepts parameters of update function )
-export let update = R.curry((connectionString, collectionName, query, update) => {
-    return helpers.funcBuilder2(updateDoc, helpers.createInputFn(query), update, connectionString, collectionName);
-});
+export let update = R.curry(
+  (connectionString, collectionName, query, update) => {
+    return helpers.funcBuilder2(
+      updateDoc,
+      helpers.createInputFn(query),
+      update,
+      connectionString,
+      collectionName
+    );
+  }
+);
 
 //input: String, String
 //output: Function
 export let dropCollection = R.curry((connectionString, collectionName) => {
-    return helpers.runner(drop, connectionString, collectionName);
+  return helpers.runner(drop, connectionString, collectionName);
 });
 
 //input: String, String, Object OR Function (returns Object)
 //output: Function ( accepts parameters of query function )
 export let remove = R.curry((connectionString, collectionName, query) => {
-    return helpers.funcBuilder(removeDoc, helpers.createInputFn(query), connectionString, collectionName);
+  return helpers.funcBuilder(
+    removeDoc,
+    helpers.createInputFn(query),
+    connectionString,
+    collectionName
+  );
 });
 
 //input: String, String, Object OR Function (returns Object)
 //output: Function ( accepts parameters of query function )
 export let count = R.curry((connectionString, collectionName, query) => {
-    return helpers.funcBuilder(countDoc, helpers.createInputFn(query), connectionString, collectionName);
+  return helpers.funcBuilder(
+    countDoc,
+    helpers.createInputFn(query),
+    connectionString,
+    collectionName
+  );
 });
 
 //input: String, String, String OR Function (returns String)
 //output: Function ( accepts parameters of field function )
 export let distinct = R.curry((connectionString, collectionName, field) => {
-    return helpers.funcBuilder(distinctFields, helpers.createInputFn(field), connectionString, collectionName);
+  return helpers.funcBuilder(
+    distinctFields,
+    helpers.createInputFn(field),
+    connectionString,
+    collectionName
+  );
 });
 
 export let stats = helpers.stats;
